cb81aecd9b34ffcc4ee17e2660ce9275dae2e135
jiheum <ajjarago@naver.com>
2026-06-08 12:25:59 +0900

    pip 기능 업데이트

---
+++ _07_module/_04_pypi_pip_requirements.py
@@ -0,0 +1,12 @@
+# PyPI ( python package index)
+# - python 패키지를 올리고 내려받는 공식 저장소 : 에셋 스토어같은?
+
+# pip : PyPI에서 패키지를 검색, 설치, 삭하는 도구
+# requirements.txt : 프로젝트에 필요한 패키지 목록을 적어두는 파일
+# -> 해당 패키지 목록을 이용해서 일괄 설치 기능 == "의존성 명세 파일"
+# unity : package.json 같은 기능
+
+
+
+
+
